a871a135707913a6fa7156dbac685c265fc68575
Sascha Ringström <sascha.ringstrom@redmind.se>
2020-04-12 16:16:41 +0200

    fixed bug caused when pastring a pastry with nested directories

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import findInFiles from 'find-in-files'
 import fsx from 'fs-extra'
+import glob from 'glob'
 import inquirer from 'inquirer'
 import replace from 'replace-in-file'
 import { tempDirectoryPath } from './constants'
@@ -9,7 +10,7 @@ import { CLIAnswers, StringCasings } from './types'
 import {
   getOptionalSnippetRegExp,
   getStringCasings,
-  sequencePromises
+  sequencePromises,
 } from './util/helpers'
 import log from './util/log'
 import { copy, exists, mkdir, readdir, rename, stats } from './util/promisified'
@@ -27,14 +28,14 @@ export async function getTemplates() {
 
 export function copyTemplateToTemporaryPath({
   templatePath,
-  temporaryCopyPath
+  temporaryCopyPath,
 }: CLIAnswers) {
   return copy(templatePath, temporaryCopyPath, { clobber: false })
 }
 
 export function copyTemplateToFinalpath({
   temporaryCopyPath,
-  finalCopyPath
+  finalCopyPath,
 }: CLIAnswers) {
   return copy(temporaryCopyPath, finalCopyPath, { clobber: false })
 }
@@ -60,46 +61,37 @@ export async function renameFiles(
     const filePaths = await getNestedFilePaths(temporaryCopyPath)
 
     return await Promise.all(
-      filePaths.map((path: string) =>
-        findAndReplace(
+      filePaths.map((path: string) => {
+        return findAndReplace(
           path,
           template_rename,
           variantsToRemove,
           replacementStrings
         )
-      )
+      })
     )
   } catch (error) {
     log.error('Error renaming files', error.message)
   }
 }
 
-const getNestedFilePaths = async function(dir: string, filelist?: any) {
-  try {
-    var path = path || require('path')
-    var fs = fs || require('fs'),
-      files = await readdir(dir)
-    filelist = filelist || []
-    files.forEach(function(file) {
-      if (fs.statSync(path.join(dir, file)).isDirectory()) {
-        filelist = getNestedFilePaths(path.join(dir, file), filelist)
-      } else {
-        filelist.push(path.join(dir, file))
-      }
+const getNestedFilePaths = async function (dir: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    glob(dir + '/**/*', (error, response) => {
+      if (error) return reject(error)
+      resolve(
+        response.filter((path) => /.+\..+/.test(path.split('/').reverse()[0]))
+      )
     })
-
-    return filelist
-  } catch (error) {
-    log.error('Error getting nested file paths', error.message)
-  }
+  })
 }
 
 export async function removeOptional(fileName: string, name: string) {
   await replace({
     files: fileName,
     from: getOptionalSnippetRegExp(name),
-    to: ''
-  }).catch(error => log.error(error.message))
+    to: '',
+  }).catch((error) => log.error(error.message))
 }
 
 export async function removeAllOptionalComments(fileName: string) {
@@ -108,8 +100,8 @@ export async function removeAllOptionalComments(fileName: string) {
   await replace({
     files: fileName,
     from: regExp,
-    to: ''
-  }).catch(error => log.error(error.message))
+    to: '',
+  }).catch((error) => log.error(error.message))
 }
 
 export async function findAndReplace(
@@ -120,7 +112,7 @@ export async function findAndReplace(
     default: 'PLACEHOLDER',
     lower: 'LOWER_PLACEHOLDER',
     upper: 'UPPER_PLACEHOLDER',
-    pascal: 'PASCAL_PLACEHOLDER'
+    pascal: 'PASCAL_PLACEHOLDER',
   }
 ) {
   const fileNameRegExp = new RegExp(searchStrings.default + '(?=\\.)', 'i')
@@ -137,7 +129,7 @@ export async function findAndReplace(
     }
 
     await sequencePromises(
-      variantsToRemove.map(variant => () => removeOptional(fileName, variant))
+      variantsToRemove.map((variant) => () => removeOptional(fileName, variant))
     )
 
     await removeAllOptionalComments(fileName)
@@ -147,25 +139,25 @@ export async function findAndReplace(
     await replace({
       files: fileName,
       from: upperRegExp,
-      to: replacements.upper
+      to: replacements.upper,
     })
 
     await replace({
       files: fileName,
       from: lowerRegExp,
-      to: replacements.lower
+      to: replacements.lower,
     })
 
     await replace({
       files: fileName,
       from: pascalRegExp,
-      to: replacements.pascal
+      to: replacements.pascal,
     })
 
     await replace({
       files: fileName,
       from: defaultRegExp,
-      to: replacements.default
+      to: replacements.default,
     })
   } catch (error) {
     log.error('Error replacing file names/values', error.message)
@@ -176,12 +168,12 @@ export async function removeFromFiles(files, regEx) {
   return await replace({
     files,
     from: regEx,
-    to: ''
+    to: '',
   })
 }
 
 export async function getTemplateOptionals({
-  tempDirectoryPath
+  tempDirectoryPath,
 }: CLIAnswers): Promise<string[]> {
   const files = await findInFiles.find('pastry-start', tempDirectoryPath, '.')
 
@@ -218,7 +210,7 @@ export async function getVariantsToRemove(answers: CLIAnswers) {
       questions.selected_variants(availableTemplateVariants)
     )
     variantsToRemove = availableTemplateVariants.filter(
-      variant => !selected_variants.includes(variant)
+      (variant) => !selected_variants.includes(variant)
     )
   }
 
